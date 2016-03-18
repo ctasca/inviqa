9144889dc8d6f974282d9ed612b1ac31aaaeec65
Carlo Tasca <ctasca@sessiondigital.com>
2016-03-18 19:55:22 +0000

    Add more n98 operations

--- magerun/Runner.py
+++ magerun/Runner.py
@@ -4,7 +4,7 @@ import re,os
 
 class Runner:
     def __init__(self, path):
-        self.path = path + '/'
+        self.path = path + os.sep
     def template_hints(self):
         local(self.path +  "n98-magerun.phar dev:template-hints")
     def clean_cache(self):
@@ -16,4 +16,7 @@ class Runner:
         lcoal(self.path + "n98-magerun.phar admin:user:create")
     def setup_run(self):
         local(self.path + "n98-magerun.phar sys:set:run")
-
+    def list(self):
+        local(self.path + "n98-magerun.phar list")
+    def command(self, command):
+        local(self.path + ("n98-magerun.phar %s" % command))
